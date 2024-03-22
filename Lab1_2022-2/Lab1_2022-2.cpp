6c0a6fcaa4274f0eb3387583d9c2630450226ba6
Marjuraru <marcelo.ramos@pucp.edu.pe>
2024-03-21 21:10:08 -0500

    ajajajaj

--- Lab1_2022-2/Lab1_2022-2.cpp
+++ Lab1_2022-2/Lab1_2022-2.cpp
@@ -10,6 +10,7 @@ void read_data(int *ptr_n, int coded_word_numbers[], char coded_word[]);
 void show_decoded_word(int n, int coded_word_numbers[], char coded_word[]);
 void sort(int n, int coded_word_numbers[], char coded_word[]);
 //holaaaa marcelo
+//ghol amiguelk
 int main()
 {
     int n;
