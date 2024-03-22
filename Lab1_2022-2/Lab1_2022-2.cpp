6bf610eded861d81dd99d3f1e2d35ebc3f0f3db0
Diegox123 <a20206284@pucp.edu.pe>
2024-03-21 21:05:49 -0500

    hola marcelo

--- Lab1_2022-2/Lab1_2022-2.cpp
+++ Lab1_2022-2/Lab1_2022-2.cpp
@@ -9,7 +9,7 @@ using namespace std;
 void read_data(int *ptr_n, int coded_word_numbers[], char coded_word[]);
 void show_decoded_word(int n, int coded_word_numbers[], char coded_word[]);
 void sort(int n, int coded_word_numbers[], char coded_word[]);
-
+//holaaaa marcelo
 int main()
 {
     int n;
